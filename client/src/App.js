6db68e731892f00e32b12effaeae1f635e2be405
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-02 17:33:17 +0530

    Define Route for login and Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,24 @@
 import React, { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 import Home from './components/Home';
+import Login from './components/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 const socket = io('http://localhost:3001');
 
 const App = () => {
-  const [users, setUsers] = useState();
-  const [user, setUser] = useState();
-  const [recieverId, setRecieverId] = useState();
-  const [messages, setMessages] = useState([]);
-  const renderCount = useRef(1);
-
-  useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-    socket.on('users', (users) => setUsers(users));
-    socket.on('recieve_message', (data) => {
-      setMessages((prevMessages) => [...prevMessages, data]);
-    });
-    socket.on('exit', (data) => setUsers(data));
-  }, []);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setUser(e.target.username.value);
-    socket.emit('new_user', e.target.username.value);
-    e.target.username.value = '';
-  };
-
-  const handleSend = (e) => {
-    e.preventDefault();
-    socket.emit('new_msg', {
-      message: e.target.message.value,
-      recieverId,
-      sentBy: user,
-    });
-    e.target.message.value = '';
-  };
-
-  const usersOnline = users?.filter((item) => item.username !== user);
   return (
-    // <>
-    //   <Home />
-    // </>
     <>
-      {!user && (
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="username" required></input>
-          <button type="submit">Create</button>
-        </form>
-      )}
-      {user && (
-        <>
-          <h1>LoggedIn User: {user}</h1>
-          {usersOnline?.length > 0 && <h1>Users Online</h1>}
-          {usersOnline?.map((user) => (
-            <p key={user.id} onClick={() => setRecieverId(user.id)}>
-              {user.username}
-            </p>
-          ))}
-          <h1>Messages</h1>
-          <ul>
-            {messages.map((data) => (
-              <>
-                <span>{data.sentBy}</span>
-                <li>{data.message}</li>
-              </>
-            ))}
-          </ul>
-          <form onSubmit={handleSend}>
-            <input type="text" name="message" required></input>
-            <button type="submit">Send</button>
-          </form>
-        </>
-      )}
-      <p>I rendered {renderCount.current} times</p>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
