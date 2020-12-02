9a58fb74d376a0c47506f50505e78191309703d1
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-02 17:30:08 +0530

    Styled Input Component

---
+++ client/src/components/Input.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const InputElement = styled.input`
+  width: 100%;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default InputElement;
