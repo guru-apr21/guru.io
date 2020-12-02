7ca97dba069a45ad19fd6bcb3ee024fe027f2956
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-02 17:31:26 +0530

    Style Message Component Elements

---
+++ client/src/components/MessageComposer/MessageComposerElements.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const MessageComposerLayout = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  border-top: 1px solid black;
+`;
+
+export const MessageComposerWrapper = styled.div`
+  height: 50px;
+  width: 100%;
+  padding-left: 82px;
+`;
+
+export const MessageCompsoserRow = styled.div`
+  display: flex;
+  height: 50px;
+  flex-direction: row;
+  margin: auto 0;
+`;
+
+// export const MessageComposerInput = styled.div`
+//   width: 100%;
+//   height: 50px;
+//   padding: 3px 0;
+//   margin: 0 auto;
+//   max-height: 140px;
+//   overflow-y: auto;
+// `;
